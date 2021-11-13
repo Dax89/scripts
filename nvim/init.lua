e6c00fe57c3f6b12847611669df8a4ede9658800
Dax <contact@antoniodavide.dev>
2021-11-13 22:38:23 +0100

    Split NeoVim configuration

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-local hotpotpath = vim.fn.stdpath("data").."/site/pack/packer/start/hotpot.nvim"
-
-if vim.fn.empty(vim.fn.glob(hotpotpath)) > 0 then
-    print("Could not find hotpot.nvim, cloning new copy to", hotpotpath)
-    vim.fn.system({"git", "clone", "https://github.com/rktjmp/hotpot.nvim", hotpotpath})
-end
-
-require("hotpot") -- Bootstrap .fnl support
-require("config") -- Load Configuration
